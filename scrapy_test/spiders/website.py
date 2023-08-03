85f2c496ade964d6c60d607a47c48be9e79e4ecc
Jonathan Michael Pinheiro da Silva <jonathan.pinheiro@apexbrasil.com.br>
2023-08-03 17:41:46 -0300

    fix: get list urls

--- scrapy_test/spiders/website.py
+++ scrapy_test/spiders/website.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 import scrapy
-
+import re
+from urllib.parse import urljoin
+from scrapy_test.items import WebsiteItem
 
 # class WebsiteSpider(scrapy.Spider):
 #     name = "website"
@@ -10,7 +13,36 @@ import scrapy
 #         pass
 
 
+""" Recebe lista de urls via input """
+# class WebsiteSpider(scrapy.Spider):
+#     name = 'website'
 
+#     def __init__(self, *args, **kwargs):
+#         super(WebsiteSpider, self).__init__(*args, **kwargs)
+#         self.urls = kwargs.get('urls', '').splitlines()
+
+#     def start_requests(self):
+#         for url in self.urls:
+#             yield scrapy.Request(url=url, callback=self.parse)
+
+#     def parse(self, response):
+#         # Extract phone numbers
+#         phone_numbers = re.findall(r'\+?[()\d\s-]+', response.text)
+#         cleaned_phone_numbers = [re.sub(r'[^\d+()]', ' ', number).strip() for number in phone_numbers]
+
+#         # Extract logo image URLs
+#         logo_urls = response.css('img[src*=logo]::attr(src)').getall()
+
+#         # Output the results
+#         item = WebsiteItem()
+#         item['url'] = response.url
+#         item['phone_numbers'] = cleaned_phone_numbers
+#         item['logo_urls'] = [urljoin(response.url, url) for url in logo_urls]
+#         yield item
+
+
+import sys
+import scrapy
 import re
 from urllib.parse import urljoin
 from scrapy_test.items import WebsiteItem
@@ -20,11 +52,12 @@ class WebsiteSpider(scrapy.Spider):
 
     def __init__(self, *args, **kwargs):
         super(WebsiteSpider, self).__init__(*args, **kwargs)
-        self.urls = kwargs.get('urls', '').splitlines()
+        self.urls = [url.strip() for url in sys.stdin]
 
     def start_requests(self):
         for url in self.urls:
             yield scrapy.Request(url=url, callback=self.parse)
+
 
     def parse(self, response):
         # Extract phone numbers
