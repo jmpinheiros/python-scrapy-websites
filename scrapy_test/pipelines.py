2d225ed4b8da0655aea41d662339c6b64bef581c
j.mpinheiros <j.mpinheiros@gmail.com>
2023-08-17 13:06:24 -0300

    feat: pep8

--- scrapy_test/pipelines.py
+++ scrapy_test/pipelines.py
@@ -1,18 +1,8 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-# from itemadapter import ItemAdapter
-
-
-# class ScrapyTestPipeline:
-#     def process_item(self, item, spider):
-#         return item
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import json
+
 
 class JsonExportPipeline:
     def open_spider(self, spider):
